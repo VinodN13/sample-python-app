c419f6ebb90780ea9624cd3de5f0a6d34ac9cdf7
Vinod <ursvsvinod@gmail.com>
2022-04-06 15:18:34 +0530

    python file

--- src/app.py
+++ src/app.py
@@ -4,12 +4,10 @@ from flask import Flask, render_template, make_response
 
 app = Flask(__name__)
 
-
 def get_app_debug_info():
     cfg_items = {k: v for k, v in os.environ.items()}
     cfg_items['datetime'] = datetime.now().isoformat()
     return cfg_items
-
 
 @app.route('/')
 def welcome():
@@ -18,11 +16,9 @@ def welcome():
         'endpoints': ['/', '/ping', '/debug', '/debug/ui']
     }
 
-
 @app.route('/ping')
 def ping():
     return {'msg': 'pong!'}
-
 
 @app.route('/debug', methods=['GET'])
 def debug():
@@ -35,7 +31,6 @@ def debug():
 
     return response
 
-
 @app.route('/debug/ui', methods=['GET'])
 def debug_ui():
     cfg_map = get_app_debug_info()
@@ -43,12 +38,10 @@ def debug_ui():
     cfg_items = sorted([{'k': k, 'v': v} for k, v in cfg_map.items()], key=lambda x: x['k'].upper())
     return render_template('debug.html', cfg_items=cfg_items, title='Hello Python Debug!')
 
-
 @app.errorhandler(404)
 def not_found(e):
     return {'err': 'Not found!'}, 404
 
-
 if __name__ == '__main__':
-    port = os.environ.get('PORT', 5001)
+    port = os.environ.get('PORT', 3005)
     app.run(debug=True, host='0.0.0.0', port=port)
